6765ab14aeb0ec8fadea79e407441f7f50ace62a
darula-hpp <loadin@30123>
2022-07-03 11:24:09 +0200

    replace react logo with aca logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './aca.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} style={{maxWidth:150, maxHeight:150}} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://africacode.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I Love ACA
         </a>
       </header>
     </div>
